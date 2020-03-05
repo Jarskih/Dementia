7db98c14a1a560047a6ed1ce7408a9774a4769fa
Jarskih <madcook86@gmail.com>
2020-03-05 16:48:17 +0100

    Fix coin booth only

--- Assets/ShowImage.cs
+++ Assets/ShowImage.cs
@@ -1,20 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class ShowImage : MonoBehaviour
 {
 
     [SerializeField] private string _eventName;
+    [SerializeField] private int delay = 6;
+    private int counter = 0;
+
+    private bool showImage;
+    private Image _image;
+
+    private void OnEnable()
+    {
+        EventManager.StartListening(_eventName, ImageVisible());
+    }
+
+    private void OnDisable()
+    {
+        EventManager.StopListening(_eventName, ImageVisible());
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _image = GetComponent<Image>();
+        _image.enabled = false;
+    }
+    
+    private void ImageVisible()
+    {
+        showImage = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!showImage)
+        {
+            return;
+        }
     }
 }
